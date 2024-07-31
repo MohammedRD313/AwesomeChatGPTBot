012564a5e0c8e258fd430cf2740aaa204631fcd2
Mohamed Ridha <hamodeygame45@gmail.com>
2024-08-01 01:10:09 +0300

    Update remixproject.py

--- Providers/remixproject.py
+++ Providers/remixproject.py
@@ -2,19 +2,23 @@
 # -*- coding: utf-8 -*-
 
 # Standard Library Modules
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Standard Library Modules
 import random
 import string
 import json
-from typing import NoReturn, List, Dict, Union, Optional
+import typing
 
 # Related Third-Party Modules
 import requests
 
 
-# Open Ai Chat Provider 
-def remix_ai(messages: List[Dict[str, str]]) -> Union[str, NoReturn]:
+# FreeGpt4 Chat Provider
+def free_gpt_4(messages: typing.List[typing.Dict[str, str]]) -> typing.Union[str, typing.NoReturn]:
     """
-    Chat completion for remix ai 
+    Chat completion for FreeGpt4
     
     Parameters:
         messages (List[Dict[str, str]]): Conversation history
@@ -23,31 +27,46 @@ def remix_ai(messages: List[Dict[str, str]]) -> Union[str, NoReturn]:
         result (Union[str, None]): result or None in failure
     """
     # Base URL for provider API
-    url: str = "https://openai-gpt.remixproject.org/"
+    url: str = "https://api.freegpt4.tech/v1/chat/completions"
     
     # Request headers
-    headers: Dict[str, str] = {
-        "Accept": "application/json",
+    headers: typing.Dict[str, str] = {
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0",
+        "Accept": "text/event-stream",
+        "Accept-Language": "de,en-US;q=0.7,en;q=0.3",
+        "Accept-Encoding": "gzip, deflate, br",
         "Content-Type": "application/json",
-        "Accept-Language": "ar",
-        "Connection": "keep-alive",
-        "Origin": "https://remix.ethereum.org",
-        "Referer": "https://remix.ethereum.org/",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "cross-site",
-        "user-agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0",
-    }
-    
-    # Request data
-    data = {
-        "prompt": str(messages),
+        "Authorization": "Bearer fg4-5KHloX6hCWhyRnJlZUdQVDQiQSiwwZ8ysll",
+        "Connection": "keep-alive"
     }
 
+    # Request data
+    data: typing.Dict[str, typing.Any] = {
+        "messages": messages,
+        "stream": True,
+        "model": "gpt-4",
+        "temperature": 0.5,
+        "presence_penalty": 0,
+        "frequency_penalty": 0,
+        "top_p": 0.5,
+    }
+   
     # Try to send request or return None on failure
     try:
-        result = requests.post(url=url, json=data, headers=headers)
-        print(result.json())
-        return result.json()['choices'][0]['message']['content']
+        # Get result from request
+        res = requests.post(url=url, json=data, headers=headers).text
+        
+        # Get final streamed response
+        gpt_result = ""
+        for chuck in res.strip().split("\n\n")[:-1]:
+            temp = json.loads(chuck.split("data: ")[1])
+            try:
+                gpt_result += temp['choices'][0]['delta']["content"]
+            except:
+                pass
+
+        # Return response
+        return gpt_result
+        
     except:
         return None
